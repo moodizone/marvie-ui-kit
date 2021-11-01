5ea28cbf64b05d381761c0679e62548be4ae9af1
moodi <moodizone@gmail.com>
2021-11-02 01:34:19 +0330

    👌 nvm

--- component/Text/index.tsx
+++ component/Text/index.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { StyleSheet, TextProps, Text as RNText } from "react-native";
 
-const Text: React.FC<TextProps> = ({
-  children,
-  style,
-  ...otherProps
-}) => {
+const Text: React.FC<TextProps> = ({ children, style, ...otherProps }) => {
   return (
     <RNText style={[styles.text, style]} {...otherProps}>
       {children}
