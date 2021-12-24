7c31bd9c7b5eb84904d8b93506d3eb4de327171b
moodi <moodizone@gmail.com>
2021-12-24 19:53:47 +0330

    🚧 package.json for publishing

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import commonjs from "@rollup/plugin-commonjs";
+import pkg from "./package.json";
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+import copy from "rollup-plugin-copy";
+
+const tsconfigOverride = {
+  compilerOptions: {
+    jsx: "react",
+    sourceMap: true,
+    declaration: true,
+    declarationDir: "./types",
+  },
+  include: ["lib/**/*"],
+};
+const external = {
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.peerDependencies),
+};
+
+export const components = {
+  external,
+  input: "lib/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.es.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    commonjs(),
+    terser(),
+    typescript({
+      clean: true,
+      tsconfigOverride,
+    }),
+    copy({
+      targets: [{ dest: "dist", src: "./lib/package.json" }],
+    }),
+  ],
+};
+
+export default components;
