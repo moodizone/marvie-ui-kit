1370e45d8f0bdfd2db4a8b6b4d7ad5ce1b9a0f21
moodi <moodizone@gmail.com>
2021-12-24 19:55:11 +0330

    👌 export all component from one file

---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+export * from "./Legend";
+export * from "./CircularProgress";
+export * from "./Circle";
+export * from "./Card";
+export * from "./ProgressBar";
+export * from "./Text";
+export * from "./ConfigProvider";
