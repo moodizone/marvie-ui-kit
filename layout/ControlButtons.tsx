acaf68c31d99f2a959585237acb82777e3d52b02
moodi <moodizone@gmail.com>
2022-01-07 21:49:28 +0330

    🚚 rename file

--- layout/ControlButtons.tsx
+++ layout/ControlButtons.tsx
@@ -3,14 +3,12 @@ import { Button, View } from "react-native";
 
 import { useAppTranslation } from "../locales/TranslationProvider";
 import { useConfig } from "../config";
-import { useNavigation } from "@react-navigation/native";
 
-const Header = () => {
-  const navigation = useNavigation();
+const ControlButtons = () => {
   const { setLocale, locale } = useAppTranslation();
   const { setDirection, setTheme, direction, scheme, gs } = useConfig();
   return (
-    <View style={[gs.mb_3, gs.row, gs.jbetween]}>
+    <View style={[gs.my_1, gs.row, gs.jbetween]}>
       <Button
         title={scheme as string}
         onPress={() => setTheme(scheme === "light" ? "dark" : "light")}
@@ -27,9 +25,8 @@ const Header = () => {
           setLocale(locale === "en" ? "fa" : "en");
         }}
       />
-      <Button title={"BACK"} onPress={() => navigation.goBack()} />
     </View>
   );
 };
 
-export default Header;
+export default ControlButtons;
