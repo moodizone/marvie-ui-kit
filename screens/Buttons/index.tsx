b0bf7116f704141ac24956317fad478e6ff77623
moodi <moodizone@gmail.com>
2022-01-09 00:11:47 +0330

    ✨ button added

---
+++ screens/Buttons/index.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { View } from "react-native";
+
+import Layout from "../../layout";
+import Button from "../../lib/Button";
+import { useConfig } from "../../config";
+import { Colors } from "../../style/type";
+import { useAppTranslation } from "../../locales/TranslationProvider";
+import Gift from "../../assets/icons/Gift";
+
+const Buttons = () => {
+  const { gs, colors } = useConfig();
+  const { t } = useAppTranslation();
+
+  return (
+    <Layout>
+      {["red", "orange", "yellow", "green", "blue", "purple"].map((c) => (
+        <View style={[gs.mb_3]} key={c}>
+          <Button
+            icon={<Gift color={"#fff"} />}
+            title={t("weeklyProgress")}
+            titleStyles={{ textTransform: "uppercase" }}
+            type={"solid"}
+            color={c as Colors}
+            style={[gs.mb_1]}
+          />
+          <Button
+            icon={<Gift color={colors[c as Colors].i as string} />}
+            title={t("weeklyProgress")}
+            type={"outlined"}
+            color={c as Colors}
+            style={[gs.mb_1]}
+          />
+          <Button
+            icon={<Gift color={colors[c as Colors].i as string} />}
+            title={t("weeklyProgress")}
+            type={"ghost"}
+            color={c as Colors}
+            style={[gs.mb_1]}
+          />
+        </View>
+      ))}
+    </Layout>
+  );
+};
+
+export default Buttons;
