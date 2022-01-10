1456672d49b842960ffff51858506d48d87f5680
moodi <moodizone@gmail.com>
2022-01-11 02:15:41 +0330

    🎨 move styles to separated file

---
+++ lib/Button/styles.ts
@@ -0,0 +1,37 @@
+import {StyleSheet} from "react-native";
+import {ButtonDimension} from "../../style";
+
+export const styles = StyleSheet.create({
+  btn: {
+    height: ButtonDimension,
+    borderRadius: 12,
+    padding: 16,
+    borderWidth: 2,
+    opacity: 1,
+  },
+  square: {
+    width: ButtonDimension,
+    overflow: "hidden",
+  },
+  circle: {
+    width: ButtonDimension,
+    borderRadius: ButtonDimension / 2,
+  },
+  title: {
+    textAlign: "center",
+    textTransform: "capitalize",
+  },
+  disabled: {
+    opacity: 0.5,
+  },
+  outlined: {
+    backgroundColor: "transparent",
+  },
+  solid: {
+    borderColor: "transparent",
+  },
+  ghost: {
+    backgroundColor: "transparent",
+    borderColor: "transparent",
+  },
+});
