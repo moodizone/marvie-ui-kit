3ad5b256790fdf6fa28e3dcc982c077e030163fd
moodi <moodizone@gmail.com>
2022-01-11 01:38:24 +0330

    ✨ typography added

---
+++ lib/Typography/index.tsx
@@ -0,0 +1,122 @@
+import * as React from "react";
+import { Text as RNText, TextStyle } from "react-native";
+
+import { TitleType, TypographyProps, TypographyType } from "./type";
+import { styles } from "./styles";
+import { useConfig } from "../../config";
+
+const BaseTypography: React.FC<TypographyType> = ({
+  children,
+  style,
+  bold,
+  thin,
+  underlined,
+  deleted,
+  italic,
+  mark,
+  markColor,
+  link,
+  code, // TODO
+  disabled,
+  ...otherProps
+}) => {
+  const { gs } = useConfig();
+  let calculatedStyles: TextStyle[] = [];
+  if (bold) {
+    calculatedStyles.push(styles.bold);
+  } else if (thin) {
+    calculatedStyles.push(styles.thin);
+  }
+
+  if (underlined || link) {
+    calculatedStyles.push(styles.underlined);
+  } else if (deleted) {
+    calculatedStyles.push(styles.deleted);
+  }
+
+  if (italic) calculatedStyles.push(styles.italic);
+  if (mark) {
+    calculatedStyles.push({
+      backgroundColor: markColor ? markColor : "yellow",
+    });
+  }
+
+  if (disabled) calculatedStyles.push(styles.disabled);
+  else if (link) calculatedStyles.push(styles.link);
+
+  return (
+    <RNText
+      style={[styles.text, gs.textAlign, calculatedStyles, style]}
+      {...otherProps}
+    >
+      {children}
+    </RNText>
+  );
+};
+
+export const Typography: TypographyProps = ({
+  children,
+  style,
+  ...otherProps
+}) => {
+  const { gs } = useConfig();
+
+  return (
+    <BaseTypography style={[gs.textAlign, style]} {...otherProps}>
+      {children}
+    </BaseTypography>
+  );
+};
+
+export const Paragraph: React.FC<TypographyType> = ({
+  children,
+  ...otherProps
+}) => {
+  return <BaseTypography {...otherProps}>{children}</BaseTypography>;
+};
+
+export const Text: React.FC<TypographyType> = ({ children, ...otherProps }) => {
+  return <BaseTypography {...otherProps}>{children}</BaseTypography>;
+};
+
+export const Title: React.FC<TitleType> = ({
+  children,
+  style,
+  level = 5,
+  ...otherProps
+}) => {
+  let calculatedStyles: TextStyle[] = [];
+
+  switch (level) {
+    case 1:
+      calculatedStyles.push(styles.h1);
+      break;
+    case 2:
+      calculatedStyles.push(styles.h2);
+      break;
+    case 3:
+      calculatedStyles.push(styles.h3);
+      break;
+    case 4:
+      calculatedStyles.push(styles.h4);
+      break;
+    case 5:
+      calculatedStyles.push(styles.h5);
+      break;
+    default:
+      calculatedStyles.push(styles.h5);
+  }
+
+  return (
+    <BaseTypography
+      {...otherProps}
+      style={[styles.bold, calculatedStyles, style]}
+    >
+      {children}
+    </BaseTypography>
+  );
+};
+
+Typography.Paragraph = Paragraph;
+Typography.Text = Text;
+Typography.Title = Title;
