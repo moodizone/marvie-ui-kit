49c7fa226f6c7cecefd4292d38a66dd5bda74e3c
moodi <moodizone@gmail.com>
2022-01-13 16:00:44 +0330

    🎨 extend layout

--- component/Layout/Container.tsx
+++ component/Layout/Container.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, ScrollViewProps } from "react-native";
 import { useConfig } from "../../config";
 
-const Container: React.FC = ({ children, ...otherProps }) => {
+const Container: React.FC<ScrollViewProps> = ({
+  children,
+  style,
+  ...otherProps
+}) => {
   const { gs } = useConfig();
   return (
-    <ScrollView style={[gs.px_3, gs.w100, gs.h100]} {...otherProps}>
+    <ScrollView style={[gs.px_3, gs.w100, gs.h100, style]} {...otherProps}>
       {children}
     </ScrollView>
   );
--- layout/index.tsx
+++ layout/index.tsx
@@ -2,10 +2,12 @@ import * as React from "react";
 
 import Container from "../component/Layout/Container";
 import ControlButtons from "./ControlButtons";
+import { useConfig } from "../config";
 
 const Layout: React.FC = ({ children }) => {
+  const { colors } = useConfig();
   return (
-    <Container>
+    <Container style={{ backgroundColor: colors.primary.iv }}>
       <ControlButtons />
       {children}
     </Container>
